6cf0e9f87da6acdc3113af27b3f3472724b8f4a8
Vyacheslav <slavik.cola2@gmail.com>
2025-04-09 19:59:29 +0300

    m

--- lec7/approximation.cpp
+++ lec7/approximation.cpp
@@ -104,7 +104,6 @@ int main()
 {
     std::vector<double> x{1, 2, 3, 5, 6, 7, 8, 9};
     std::vector<double> u{1, 4, 4, 2, 3, 3, 4, 2};
-
     double a = vec_min(x);
     double b = vec_max(x);
     scale_x(a, b, x);
