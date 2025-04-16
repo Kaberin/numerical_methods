d9e9bc0ca323195297bae206f7a8c6d73101a39c
vyacheslav <slavik.cola2@gmail.com>
2025-04-16 14:11:16 +0300

    newton minimization

--- lec9/main.cpp
+++ lec9/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <functional>
 #include <cmath>
+
 double f(double x)
 {
-    return sin(x);
+    return (x - 6) * (x - 5) + x * x * x + 4 * x * x * x * x + x - 8;
 }
 /// @brief Поиск минимума методом золотого сечения. Не работает на симметричном отрезке (потом пофикшу)
 /// @param a Левая граница отрезка
@@ -50,11 +51,58 @@ double minimize_gr(double a, double b, std::function<double(double)> f)
     return min;
 }
 
+double differ_first(std::function<double(double)> f, double x)
+{
+    double h = 0.0001;
+    return (f(x + h) - f(x)) / h;
+}
+double differ_second(std::function<double(double)> f, double x)
+{
+    double h = 0.0001;
+    return (f(x + h) - 2 * f(x) + f(x - h)) / (h * h);
+}
+/// @brief Минимум методом ньютона. Вроде работает
+/// @param f
+/// @param eps
+/// @return
+double newton_min(std::function<double(double)> f, double eps)
+{
+    double x[50];
+    x[0] = 10000;
+    int n = 0;
+    for (n = 0; n < 49; n++)
+    {
+        std::cout << differ_first(f, x[n]) / differ_second(f, x[n]) << '\n';
+        x[n + 1] = x[n] - differ_first(f, x[n]) / differ_second(f, x[n]);
+        if (n > 2)
+        {
+            double cond = fabs((x[n] - x[n - 1]) / (1 - (x[n] - x[n - 1]) / (x[n - 1] - x[n - 2])));
+            if (cond <= eps)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    std::cout << x[i] << '\t';
+                }
+                return x[n + 1];
+            }
+        }
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        std::cout << x[i] << '\t';
+    }
+    std::cout << "Не достигнута требуемая точность. *dies from cringe*";
+    return x[n];
+}
 int main()
 {
     const double pi = 3.1415926;
-    double res = minimize_gr(0, 2 * pi, f);
-
+    double res = minimize_gr(-3, 2 * pi, f);
     std::cout << "Function min at " << res << '\n';
+    res = newton_min(f, 1e-9);
+    std::cout << "Function min at " << res << '\n';
+    // std::cout << differ_first(f, 1) << '\n';
+    // std::cout << differ_second(f, 1) << '\n';
+
     return 0;
 }
